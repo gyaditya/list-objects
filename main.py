7ae8e0eed81c9bfb4b2473d9f0ca008ced13f6de
gyaditya <112659420+gyaditya@users.noreply.github.com>
2022-12-01 14:44:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,46 @@ dataStr = file.read()
 file.close()
 
 color_data = json.loads(dataStr)
+
+#First
+def nameFam(anArray):
+    for i in range(len(anArray)):
+        print("The color is: " + anArray[i]["name"])
+        print("The family of the color is, " + anArray[i]["family"])
+
+
+#Second
+def nameBright(anArray):
+    for i in range(len(anArray)):
+        brightness = anArray[i]["brightness"]
+        if brightness >= 200:
+            print(anArray[i]["name"]  + "With a brightness of, " + str(brightness))
+
+#Third
+def redPink(anArray):
+    count = 0
+    for i in range(len(anArray)):
+        family = anArray[i]["family"]
+        if family == "Red" or family == "Pink":
+            count += 1
+    print(count)
+
+
+#Fourth
+def  colorUser(anArray):
+    userInput = input("Please enter the name Of the Family:\n")
+    count = 0
+    print("For the family, " + userInput + ". These are the colors:")
+    for i in range(len(anArray)):
+        if anArray[i]["family"] == userInput:
+            print(anArray[i]["name"])
+            count += 1
+    print("The total count is: " + str(count))
+
+
+#Fifth
+def letterUser(anArray):
+    userInput = input("What is your letter:\n").upper()
+    for i in range(len(anArray)):
+        if anArray[i]["name"].startswith(userInput):
+            print(anArray[i]["name"])
